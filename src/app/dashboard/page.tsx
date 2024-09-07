4b278900339606639b9710981375063ef4cbf18e
arsalan-khoso <ahmedarslan599@gmail.com>
2024-09-07 16:54:53 +0500

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard Component</h1>
+    </div>
+  );
+};
+
+export default dashboard;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <h1>
+      Here is the Main Page
+    </h1>
+  );
+}
